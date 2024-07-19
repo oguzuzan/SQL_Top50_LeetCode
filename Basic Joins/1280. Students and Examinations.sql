4543280a3361079de0526e1e7c6f9bc8438da040
Oguz Enver Uzan <158345675+oguzuzan@users.noreply.github.com>
2024-07-19 17:21:15 +0300

    1280. Students and Examinations.sql

---
+++ Basic Joins/1280. Students and Examinations.sql
@@ -0,0 +1,8 @@
+SELECT s.student_id, s.student_name, sub.subject_name, COUNT(e.subject_name) AS attended_exams
+FROM Students AS s
+JOIN Subjects AS sub
+LEFT JOIN Examinations AS e
+ON s.student_id = e.student_id 
+AND sub.subject_name = e.subject_name
+GROUP BY s.student_id, sub.subject_name
+ORDER BY s.student_id, sub.subject_name
