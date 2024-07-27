ac84d46b8b4b7b1e0a4b1fdae90d7abd47b991b7
Oguz Enver Uzan <158345675+oguzuzan@users.noreply.github.com>
2024-07-27 13:58:44 +0300

    1193. Monthly Transactions I.sql

---
+++ Basic Aggregate Functions/1193. Monthly Transactions I.sql
@@ -0,0 +1,7 @@
+SELECT CONCAT(YEAR(trans_date), '-', LPAD(MONTH(trans_date), 2,'0')) AS month,
+country, COUNT(*) AS trans_count, 
+SUM(CASE WHEN state = 'approved' THEN 1 ELSE 0 END) AS approved_count, 
+SUM(amount) AS trans_total_amount,
+SUM(CASE WHEN state = 'approved' THEN amount ELSE 0 END) AS approved_total_amount
+FROM Transactions
+GROUP BY month, country
